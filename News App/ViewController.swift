49a54baf04c455aef1ea8f348da38cc2615215e6
Chris Boshoff <boshoff7@gmail.com>
2022-02-25 10:03:28 +0200

    Initial Commit

---
+++ News App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News App
+//
+//  Created by Chris Boshoff on 2022/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
